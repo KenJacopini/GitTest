c0373f9b2b1f6028b0cc60b07bcc147d1b36840b
Nwobodo Kenneth <kennethgranero@gmail.com>
2019-05-14 15:02:20 +0100

    i just wrote my book class

---
+++ Book.java
@@ -0,0 +1,17 @@
+public class Book{
+
+	private int pages;
+	private String author;
+	private String genre;
+	private boolean isHardcover;
+
+	public static void main(String[] args){
+
+		Book 
+	
+	}
+
+
+
+
+}
